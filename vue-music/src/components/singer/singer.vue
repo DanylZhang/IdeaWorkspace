a99233b5f017fcf240d1b1afc4aed2e2e87ca4b0
danyl <1475811550@qq.com>
2018-03-19 08:01:50 +0800

    add api/singer.js

--- vue-music/src/components/singer/singer.vue
+++ vue-music/src/components/singer/singer.vue
@@ -3,11 +3,35 @@
 </template>
 
 <script>
+  import {getSingerList} from '../../api/singer';
+  import {ERR_OK} from '../../api/config';
+
   export default {
-    name: 'singer'
+    name: 'singer',
+    data() {
+      return {
+        singers: []
+      };
+    },
+    created() {
+      this._getSingerList();
+    },
+    methods: {
+      _getSingerList() {
+        getSingerList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.singers = res.data.list;
+          }
+        });
+      }
+    }
   };
 </script>
 
-<style scoped>
-
+<style lang="stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    widows: 100%
 </style>
