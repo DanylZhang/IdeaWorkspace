60876d4fe5392bda8fe791505fcabd79dfd52f88
zhang.danyu <zhang.danyu@sh.adways.net>
2018-06-01 18:39:33 +0800

    add forkjoin demo

---
+++ learn-java/src/main/java/com/danyl/learnjava/lscjszconcurrency/ch5/CountDownLatchDemo.java
@@ -0,0 +1,37 @@
+package com.danyl.learnjava.lscjszconcurrency.ch5;
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CountDownLatchDemo implements Runnable {
+    // CountDownLatch常用于一个主线程等待其余工作线程的完成
+    static final CountDownLatch end = new CountDownLatch(10);
+    static final CountDownLatchDemo demo = new CountDownLatchDemo();
+
+    @Override
+    public void run() {
+        try {
+            // 模拟检查任务
+            Thread.sleep(new Random().nextInt(10) * 1000);
+            System.out.println("check complete");
+            end.countDown();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 10; i++) {
+            executorService.submit(demo);
+        }
+
+        // 等待检查完毕
+        end.await();
+        // 发射火箭
+        System.out.println("Fire!");
+        executorService.shutdown();
+    }
+}
--- learn-java/src/main/java/com/danyl/learnjava/lscjszconcurrency/ch5/ReenterLock2.java
+++ learn-java/src/main/java/com/danyl/learnjava/lscjszconcurrency/ch5/ReenterLock2.java
@@ -2,7 +2,7 @@ package com.danyl.learnjava.lscjszconcurrency.ch5;
 
 import java.util.concurrent.locks.ReentrantLock;
 
-public class ReenterLock implements Runnable {
+public class ReenterLock2 implements Runnable {
 
     public static ReentrantLock lock = new ReentrantLock();
 
@@ -12,18 +12,20 @@ public class ReenterLock implements Runnable {
     public void run() {
         for (int j = 0; j < 10000000; j++) {
             lock.lock();
+            lock.lock();
             try {
                 i++;
             } catch (Exception e) {
                 e.printStackTrace();
             } finally {
                 lock.unlock();
+                lock.unlock();
             }
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
-        ReenterLock reenterLock = new ReenterLock();
+        ReenterLock2 reenterLock = new ReenterLock2();
         Thread t1 = new Thread(reenterLock);
         Thread t2 = new Thread(reenterLock);
         t1.start();
