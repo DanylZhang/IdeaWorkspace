e3ec67c48ec912afe871f55225806c591a4698db
zhang.danyu <zhang.danyu@sh.adways.net>
2018-06-06 16:33:01 +0800

    finish lscjconcurrency

--- prometheus/src/main/java/transfer/IAccountDAO.java
+++
@@ -1,9 +0,0 @@
-package transfer;
-
-/**
- * Created by Administrator on 2017-6-16.
- */
-public interface IAccountDAO {
-    void transin(Long id,double amount);
-    void transout(Long id,double amount);
-}
