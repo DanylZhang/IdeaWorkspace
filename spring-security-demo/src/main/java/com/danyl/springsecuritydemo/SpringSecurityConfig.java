178c0aa6300f207aa05559dfe5acd73d045ab947
danyl <1475811550@qq.com>
2018-07-01 22:40:22 +0800

    add SpringSecurityConfig auth.inMemory

--- spring-security-demo/src/main/java/com/danyl/springsecuritydemo/SpringSecurityConfig.java
+++ spring-security-demo/src/main/java/com/danyl/springsecuritydemo/SpringSecurityConfig.java
@@ -1,10 +1,21 @@
 package com.danyl.springsecuritydemo;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
+@Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance()).withUser("admin").password("123456").roles("ADMIN");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -14,11 +25,11 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout().permitAll()
                 .and().formLogin();
-
+        http.csrf().disable();
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/js/**","/css/**","/images/**");
+        web.ignoring().antMatchers("/js/**", "/css/**", "/images/**");
     }
 }
--- spring-security-demo/src/main/java/com/danyl/springsecuritydemo/SpringSecurityDemoApplication.java
+++ spring-security-demo/src/main/java/com/danyl/springsecuritydemo/SpringSecurityDemoApplication.java
@@ -3,6 +3,7 @@ package com.danyl.springsecuritydemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,10 @@ public class SpringSecurityDemoApplication {
     @RequestMapping("/")
     public String home() {
         return "hello spring boot";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello world";
     }
 }
